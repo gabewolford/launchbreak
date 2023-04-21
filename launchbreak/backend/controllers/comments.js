07645adeea6deea2cfc7d67eeb44d0dd291ec5d0
gabewolford <gabewolford@gmail.com>
2023-04-21 11:09:38 -0700

    reinstalled tailwind, added router to controller

---
+++ launchbreak/backend/controllers/comments.js
@@ -0,0 +1,27 @@
+/* 
+---------------------------------------------------------------------------------------
+NOTE: Remember that all routes on this page are prefixed with `localhost:3000/api/comments`
+---------------------------------------------------------------------------------------
+*/
+
+
+/* Require modules
+--------------------------------------------------------------- */
+const express = require('express')
+// Router allows us to handle routing outisde of server.js
+const router = express.Router()
+
+
+/* Require the db connection, and models
+--------------------------------------------------------------- */
+const db = require('../models')
+
+
+/* Routes
+--------------------------------------------------------------- */
+
+
+
+/* Export these routes so that they are accessible in `server.js`
+--------------------------------------------------------------- */
+module.exports = router
