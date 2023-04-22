d9ecb4fa685c980d4fbff8ccd22b0238ae788871
gabewolford <gabewolford@gmail.com>
2023-04-21 22:32:50 -0700

    done for the night

--- launchbreak/frontend/src/components/Header/index.jsx
+++ launchbreak/frontend/src/components/Header/index.jsx
@@ -7,8 +7,8 @@ export default function Header() {
         <nav className="flex items-center justify-between flex-wrap p-4">
             <div className="flex items-center flex-shrink-0 text-white mr-6">
                 <NavLink 
-                    to="/"
-                    className="text-white font-semibold text-2xl ml-2 tracking-tight hover:text-orange-300 hover:cursor-pointer"
+                to="/"
+                className="text-white font-semibold text-2xl ml-2 tracking-tight hover:text-orange-400 hover:cursor-pointer transition ease-in-out duration-400"
                 >
                     <p>Launchbreak</p>
                 </NavLink>
@@ -20,40 +20,44 @@ export default function Header() {
             </div>
             <div className="w-full block flex-end md:flex md:items-center md:w-auto">
                 <div className="text-sm md:flex-grow w-full block flex-grow md:flex md:items-center md:w-auto">
-                    <div className="mr-4">
+                    <div className="">
                         <ul>
                         <NavLink 
-                            to="/launches"
-                            className="block mt-4 font-semibold md:inline-block md:mt-0 text-white hover:text-orange-300 mx-4"
+                        to="/launches"
+                        className="block mt-4 font-semibold md:inline-block md:mt-0 text-white hover:text-orange-400 hover:scale-105 transition ease-in-out duration-400 mx-4"
                         >
                             <li>Launches</li>
                         </NavLink>
                         <NavLink 
-                            to="/news"
-                            className="block mt-4 font-semibold md:inline-block md:mt-0 text-white hover:text-orange-300 mx-4"
+                        to="/news"
+                        className="block mt-4 font-semibold md:inline-block md:mt-0 text-white hover:text-orange-400 hover:scale-105 transition ease-in-out duration-400 mx-4"
                         >
                             <li>News</li>
                         </NavLink>
                         <NavLink 
-                            to="/agencies"
-                            className="block mt-4 font-semibold md:inline-block md:mt-0 text-white hover:text-orange-300 mx-4"
+                        to="/agencies"
+                        className="block mt-4 font-semibold md:inline-block md:mt-0 text-white hover:text-orange-400 hover:scale-105 transition ease-in-out duration-400 mx-4"
                         >
                             <li>Agencies</li>
                         </NavLink>
                         <NavLink 
-                            to="/spacecraft"
-                            className="block mt-4 font-semibold md:inline-block md:mt-0 text-white hover:text-orange-300 mx-4"
+                        to="/spacecraft"
+                        className="block mt-4 font-semibold md:inline-block md:mt-0 text-white hover:text-orange-400 hover:scale-105 transition ease-in-out duration-400 mx-4"
                         >
                             <li>Spacecraft</li>
                         </NavLink>
                         <NavLink 
-                            to="/astronauts"
-                            className="block mt-4 font-semibold md:inline-block md:mt-0 text-white hover:text-orange-300 mx-4"
+                        to="/astronauts"
+                        className="block mt-4 font-semibold md:inline-block md:mt-0 text-white hover:text-orange-400 hover:scale-105 transition ease-in-out duration-400 mx-4"
                         >
                             <li>Astronauts</li>
                         </NavLink>
-                        <NavLink to="/account">
-                            <img src="../src/assets/space-station.svg" alt="account" className="block mt-4 font-semibold md:inline-block md:mt-0 text-white hover:text-orange-300 ml-4"/>
+                        <NavLink 
+                        to="/account">
+                            <img 
+                                src="../src/assets/space-station.svg" 
+                                alt="account" 
+                                className="block mt-4 font-semibold md:inline-block md:mt-0 text-white  hover:scale-125 transition ease-in-out duration-500 mx-4"/>
                         </NavLink>
                         </ul>
                     </div>
