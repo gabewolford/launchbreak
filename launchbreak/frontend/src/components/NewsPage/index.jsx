1af8286d4fb0af2ef45172ef2b54adcf09056e6c
gabewolford <gabewolford@gmail.com>
2023-04-26 17:25:07 -0700

    increased grid gap on news index page

--- launchbreak/frontend/src/components/NewsPage/index.jsx
+++ launchbreak/frontend/src/components/NewsPage/index.jsx
@@ -6,7 +6,7 @@ export default function NewsPage({ newsList }) {
         <>
         <div className="lg:mx-12">
             <h1 className="text-3xl font-bold text-white m-6">Recent News</h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 mx-6 gap-5">
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 mx-6 gap-10">
                 {newsList}
             </div>
         </div>
