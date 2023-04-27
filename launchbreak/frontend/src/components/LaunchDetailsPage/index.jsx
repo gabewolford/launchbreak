503ab14ce100ac56ff5a14608c51fa08f029f582
gabewolford <gabewolford@gmail.com>
2023-04-26 22:36:37 -0700

    finalized conditionals for launch data

--- launchbreak/frontend/src/components/LaunchDetailsPage/index.jsx
+++ launchbreak/frontend/src/components/LaunchDetailsPage/index.jsx
@@ -54,81 +54,107 @@ export default function LaunchDetailsPage({ launchData, updateDetails, detailPag
                             </iframe>
                         </div>
 
-                        <div id='launch-details' className='lg:w-1/2'>
-                            <div className="px-4 pt-4">
-                                <h2 className="text-xl font-bold mb-1">Rocket Details</h2>
-                                <p className='mb-4 text-sm'>{launchData.rocket.configuration?.description}</p>
-                                <table className=''>
-                                    <div className="border-2 border-solid p-1 border-gray-400">
-                                        <th className='text-blue-300 text-lg pl-2 pb-1 text-left'>
-                                            Configuration
-                                        </th>
-                                        <tr className='bg-white'>
-                                            <td className='italic text-gray-900 pl-2'>Name</td>
-                                            <td className='pl-5 pr-2 text-gray-900'>{launchData.rocket.configuration?.full_name}</td>
+                        <div className="h-fit lg:w-1/2">
+                            <section id='config-details' className="h-fit flex">
+                                <table className="w-full text-sm text-left mx-4 mb-4">
+                                    <thead className="text-lg">
+                                        <tr>
+                                            <th scope="col" className="py-3">
+                                                Rocket Details
+                                            </th>
                                         </tr>
-                                        <tr className=''>
-                                            <td className='italic pl-2'>Family</td>
-                                            <td className='pl-5'>{launchData.rocket.configuration?.family}</td>
+                                    </thead>
+                                    <tbody className='px-4 border-4 border-blue-300'>
+                                        <tr className="bg-white border-b">
+                                            <td scope="row" className="px-4 py-2 text-black italic font-bold border-r">
+                                                Name
+                                            </td>
+                                            {launchData.rocket.configuration.full_name ? <td className="px-4 py-2 text-black">{launchData.rocket.configuration?.full_name}</td> : <td className="px-4 py-2 text-black">N/A</td>}
                                         </tr>
-                                        <tr className='bg-white'>
-                                            <td className='italic text-gray-900 pl-2'>Variant</td>
-                                            <td className='pl-5 text-gray-900'>{launchData.rocket.configuration?.variant}</td>
+                                        <tr className="border-b bg-gray-100">
+                                            <th scope="row" className="px-4 py-2 text-black italic font-bold border-r">
+                                                Family
+                                            </th>
+                                            {launchData.rocket.configuration.family ? <td className="px-4 py-2 text-black">{launchData.rocket.configuration.family}</td> : <td className="px-4 py-2 text-black">N/A</td>}
+
                                         </tr>
-                                        <tr className=''>
-                                            <td className='italic pl-2'>Stages</td>
-                                            <td className='pl-5'>{launchData.rocket.configuration?.max_stage}</td>
+                                        <tr className="bg-white border-b">
+                                            <td scope="row" className="px-4 py-2 text-black italic font-bold border-r">
+                                                Variant
+                                            </td>
+                                            {launchData.rocket.configuration.variant ? <td className="px-4 py-2 text-black">{launchData.rocket.configuration.variant}</td> : <td className="px-4 py-2 text-black">N/A</td>}
                                         </tr>
-                                        <tr className='bg-white'>
-                                            <td className='italic text-gray-900 pl-2'>Reusable</td>
-                                            <td className='pl-5 text-gray-900'>{launchData.rocket.configuration?.reusable == true && 'Yes'}</td>
+                                        <tr className="border-b bg-gray-100">
+                                            <th scope="row" className="px-4 py-2 text-black italic font-bold border-r">
+                                                Stages
+                                            </th>
+                                            {launchData.rocket.configuration.max_stages ? <td className="px-4 py-2 text-black">{launchData.rocket.configuration.max_stages}</td> : <td className="px-4 py-2 text-black">N/A</td>}
                                         </tr>
-                                        <tr className=''>
-                                            <td className='italic pl-2'>Length (m)</td>
-                                            <td className='pl-5'>{launchData.rocket.configuration?.length}</td>
+                                        <tr className="bg-white border-b">
+                                            <td scope="row" className="px-4 py-2 text-black italic font-bold border-r">
+                                                Reusable
+                                            </td>
+                                            {(launchData.rocket.configuration.reusable && launchData.rocket.configuration.reusable === true) && <td className="px-4 py-2 text-black">Yes</td>}
+                                            {(launchData.rocket.configuration.reusable && launchData.rocket.configuration.reusable === false) && <td className="px-4 py-2 text-black">No</td>}
+                                            {(!launchData.rocket.configuration.reusable) && <td className="px-4 py-2 text-black">N/A</td>}
                                         </tr>
-                                        <tr className='bg-white'>
-                                            <td className='italic text-gray-900 pl-2'>Diameter (m)</td>
-                                            <td className='pl-5 text-gray-900'>{launchData.rocket.configuration?.diameter}</td>
+                                        <tr className="border-b bg-gray-100">
+                                            <th scope="row" className="px-4 py-2 text-black italic font-bold border-r">
+                                                Length (m)
+                                            </th>
+                                            {launchData.rocket.configuration.length ? <td className="px-4 py-2 text-black">{launchData.rocket.configuration.length}</td> : <td className="px-4 py-2 text-black">N/A</td>}
                                         </tr>
-                                        <tr className=''>
-                                            <td className='italic pl-2'>Launch Mass (T)</td>
-                                            <td className='pl-5'>{launchData.rocket.configuration.launch_mass?.toLocaleString()}</td>
+                                        <tr className="bg-white border-b">
+                                            <td scope="row" className="px-4 py-2 text-black italic font-bold border-r">
+                                                Diameter (m)
+                                            </td>
+                                            {launchData.rocket.configuration.diameter ? <td className="px-4 py-2 text-black">{launchData.rocket.configuration.diameter}</td> : <td className="px-4 py-2 text-black">N/A</td>}
                                         </tr>
-                                        <tr className='bg-white'>
-                                            <td className='italic text-gray-900 pl-2'>LEO Capcity (kg)</td>
-                                            <td className='pl-5 text-gray-900'>{launchData.rocket.configuration.leo_capacity?.toLocaleString()}</td>
+                                        <tr className="border-b bg-gray-100">
+                                            <th scope="row" className="px-4 py-2 text-black italic font-bold border-r">
+                                                Launch Mass (T) 
+                                            </th>
+                                            {launchData.rocket.configuration.launch_mass ? <td className="px-4 py-2 text-black">{launchData.rocket.configuration.launch_mass.toLocaleString()}</td> : <td className="px-4 py-2 text-black">N/A</td>}
                                         </tr>
-                                        <tr className=''>
-                                            <td className='italic pl-2'>GTO Capcity (kg)</td>
-                                            <td className='pl-5'>{launchData.rocket.configuration.gto_capacity?.toLocaleString()}</td>
+                                        <tr className="bg-white border-b">
+                                            <td scope="row" className="px-4 py-2 text-black italic font-bold border-r">
+                                                LEO Capcity (kg)
+                                            </td>
+                                            {launchData.rocket.configuration.leo_capacity ? <td className="px-4 py-2 text-black">{launchData.rocket.configuration.leo_capacity.toLocaleString()}</td> : <td className="px-4 py-2 text-black">N/A</td>}
                                         </tr>
-                                        <tr className='bg-white'>
-                                            <td className='italic text-gray-900 pl-2'>Liftoff Thrust (kN)</td>
-                                            <td className='pl-5 text-gray-900'>{launchData.rocket.configuration?.to_thrust?.toLocaleString()}</td>
+                                        <tr className="border-b bg-gray-100">
+                                            <th scope="row" className="px-4 py-2 text-black italic font-bold border-r">
+                                                GTO Capcity (kg)
+                                            </th>
+                                            {launchData.rocket.configuration.gto_capacity ? <td className="px-4 py-2 text-black">{launchData.rocket.configuration.gto_capacity.toLocaleString()}</td> : <td className="px-4 py-2 text-black">N/A</td>}
                                         </tr>
-                                    </div>
+                                        <tr className="border-b bg-gray-100">
+                                            <th scope="row" className="px-4 py-2 text-black italic font-bold border-r">
+                                                Liftoff Thrust (kN)
+                                            </th>
+                                            {launchData.rocket.configuration.to_thrust ? <td className="px-4 py-2 text-black">{launchData.rocket.configuration.to_thrust.toLocaleString()}</td> : <td className="px-4 py-2 text-black">N/A</td>}
+                                        </tr>
+                                    </tbody>
                                 </table>
-                            </div>
-                            
-                            <div className="w-full p-4">
-                            </div>
+                            </section>
                         </div>
                     </div>
 
-                    <div className='mx-12 max-h-fit flex flex-col lg:flex-row lg:max-w-[75vw] lg:mx-auto'>
-                        <figure>
-                            <img src={launchData.mission_patches[0]?.image_url} className="w-full lg:max-h-80"/>
-                            <figcaption className='text-gray-400 text-center'>{launchData.mission_patches[0]?.name}</figcaption>
-                        </figure>
-
-                    </div>
+                    
 
                     <hr className='my-12 mx-6'/>
 
-                    <div className='mx-12 lg:max-w-[75vw] lg:mx-auto'>
-                        <h1 className='text-2xl font-bold m-5'>Comments</h1>
-                        <CommentSection launchId={detailPageId}/>
+                    <div className='mx-12 max-h-fit flex flex-col lg:flex-row lg:max-w-[75vw] lg:mx-auto'>
+                        <div className="lg:w-1/2">
+                            <h1 className='text-2xl font-bold mb-5'>Comments</h1>
+                            <CommentSection launchId={detailPageId}/>
+                        </div>
+
+                        <figure className='lg:w-1/2 p-8'>
+                            <img src={launchData.mission_patches[0]?.image_url} className="w-full lg:max-h-80"/>
+                            <figcaption className='text-white text-center'>{launchData.mission_patches[0]?.name}</figcaption>
+                        </figure>
+
                     </div>
                 </>
     }
