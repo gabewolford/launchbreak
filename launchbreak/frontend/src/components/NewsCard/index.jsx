8f72b407cc2632c35a9e5d118fb981101f7bd751
gabewolford <gabewolford@gmail.com>
2023-04-27 10:11:41 -0700

    restyled news card

--- launchbreak/frontend/src/components/NewsCard/index.jsx
+++ launchbreak/frontend/src/components/NewsCard/index.jsx
@@ -1,16 +1,17 @@
 export default function NewsCard({ newsData }) {
     return (
         <>
-            <div className="bg-gray-500">
-                <img src={newsData.image_url}/>
+            <div className="bg-gray-500 rounded-lg">
+                <img src={newsData.image_url} alt="Article image" className="mt-8"/>
                 <div className="p-3">
-                    <h2 className="text-2xl font-bold pt-3">{newsData.title}</h2>
+                    <h2 className="text-2xl font-bold">{newsData.title}</h2>
                     <p className="text-xs text-blue-300 pt-2 mb-3">Published {new Date(newsData.published_at).toLocaleString()}</p>
-                    <p className="mb-5">{newsData.summary}</p>
-                    <a href={newsData.url} target="_blank">
-                        <span className="font-bold bg-blue-900 hover:bg-blue-800 text-sm py-1 px-2 rounded">Read More</span>
-                    </a>
-                    
+                    <p className="mb-4">{newsData.summary}</p>
+                    <div className="mb-1 text-left">
+                        <a href={newsData.url} target="_blank">
+                            <span className="font-bold bg-blue-900 hover:bg-blue-800 text-md py-1 px-2 rounded">Read More</span>
+                        </a>
+                    </div>
                 </div>
                 
             </div>
