830d28d52fea849e9ab8ddbb85815a51b71d2780
gabewolford <gabewolford@gmail.com>
2023-04-27 20:58:35 -0700

    agencies index page updated

--- launchbreak/frontend/src/components/AgenciesPage/index.jsx
+++ launchbreak/frontend/src/components/AgenciesPage/index.jsx
@@ -1,12 +1,12 @@
 export default function AgenciesPage({ agencyList }) {
-
-
     return (
         <>
-            <div className="">
-                {agencyList}
+            <div className="mx-12">
+                <h1 className="mt-6 mb-12 text-3xl font-bold text-white">Agencies</h1>
+                    <div className="grid grid-cols-1 lg:w-[70vw] mx-auto">
+                        {agencyList}
+                    </div>
             </div>
-            
         </>
     )
 }
