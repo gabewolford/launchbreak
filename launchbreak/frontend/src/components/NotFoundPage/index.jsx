5b0be3f952fd45ae39d8e31f5ec1d1014c66ffdc
gabewolford <gabewolford@gmail.com>
2023-04-28 12:31:23 -0700

    404 page looking good, link back to home page added

--- launchbreak/frontend/src/components/NotFoundPage/index.jsx
+++ launchbreak/frontend/src/components/NotFoundPage/index.jsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom"
+
 export default function NotFoundPage() {
     return (
         <>
-            <h1>Not Found Page</h1>
+            <div className="flex items-center justify-center h-screen">
+                <div className="text-center">
+                    <h1 className="text-5xl mb-4">404 ERROR</h1>
+                    <h4 className="text-xl mb-6">Sorry, this page does not exist.</h4>
+                    <button>
+                        <Link to="/">
+                            <p className="bg-orange-500 hover:bg-orange-400 px-4 py-2 rounded-lg font-bold">GO HOME</p>
+                        </Link>
+                    </button>   
+                </div>
+            </div>
         </>
     )
 }
